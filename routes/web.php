f08fd259b1ab4232d5c827f6d8ed339b4bc08070
ZeinaZayed4 <zeinazayed@std.mans.edu.eg>
2024-12-26 16:09:26 +0200

    Learn middleware

--- routes/web.php
+++ routes/web.php
@@ -4,6 +4,7 @@ use App\Http\Controllers\PostController;
 use App\Http\Controllers\SingleController;
 use App\Http\Controllers\TestController;
 use App\Http\Controllers\Users\UserController;
+use App\Http\Middleware\CheckIfNameIsZeina;
 use Illuminate\Support\Facades\Route;
 
 /*
@@ -17,11 +18,11 @@ use Illuminate\Support\Facades\Route;
 |
 */
 
-Route::get('/', function () {
-    return view('index');
-});
+//Route::get('/', function () {
+//    return view('index');
+//});
 
-Route::get('/zeina', [TestController::class, 'zeina']);
+//Route::get('/zeina', [TestController::class, 'zeina']);
 
 // Shorthand routes
 /*
@@ -29,14 +30,21 @@ Route::view('/zeina', 'index');
 Route::redirect('/here', 'https://google.com');
 */
 
-Route::get('/print-name/{name?}', [TestController::class, 'printName'])->name('print-my-name-route');
+//Route::get('/print-name/{name?}', [TestController::class, 'printName'])->name('print-my-name-route');
 
 // Routes groups
-//Route::controller(TestController::class)->prefix('test')->name('test.')->group(function () {
-//    Route::get('/zeina', 'zeina');
-//    Route::get('/print-name/{name?}', 'printName')->name('print-my-name-route');
-//});
+Route::controller(TestController::class)->prefix('test')->name('test.')
+    ->middleware('check_zeina')->group(function () {
+        Route::get('/zeina', 'zeina');
+        Route::get('/print-name/{name?}', 'printName')->name('print-my-name-route');
+});
 
 //Route::get('/hello', [UserController::class, 'index']);
-//Route::get('/hello', SingleController::class);
-Route::resource('posts', PostController::class)->only(['index', 'show']);
+
+//Route::resource('posts', PostController::class)->only(['index', 'show']);
+
+// Middleware
+//Route::get('/hello', SingleController::class)
+//    ->middleware('check_zeina');
+
+Route::get('/hello', SingleController::class);
