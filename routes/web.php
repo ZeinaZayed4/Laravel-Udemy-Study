7bc0c502c60f11fc81bbff7f55adbdc2a43dde8f
ZeinaZayed4 <zeinazayed@std.mans.edu.eg>
2025-01-07 13:54:16 +0200

    Learn authentication

--- routes/web.php
+++ routes/web.php
@@ -1,58 +1,31 @@
 <?php
 
-use App\Http\Controllers\PostController;
-use App\Http\Controllers\SingleController;
-use App\Http\Controllers\TestController;
-use App\Http\Controllers\ThemeController;
-use App\Http\Controllers\UserController;
-use App\Http\Middleware\CheckIfNameIsZeina;
+use App\Http\Controllers\ProfileController;
 use Illuminate\Support\Facades\Route;
 
-//Route::get('/', function () {
-//    return view('zeina');
-//});
-
-//Route::get('/', function () {
-//    $data = 'Zeina';
-//   return view('posts.create.index.zeina', compact('data'));
-//});
-
-//Route::get('/', function () {
-//    return view('index');
-//});
-
-//Route::get('/zeina', [TestController::class, 'zeina']);
-
-// Shorthand routes
 /*
-Route::view('/zeina', 'index');
-Route::redirect('/here', 'https://google.com');
+|--------------------------------------------------------------------------
+| Web Routes
+|--------------------------------------------------------------------------
+|
+| Here is where you can register web routes for your application. These
+| routes are loaded by the RouteServiceProvider and all of them will
+| be assigned to the "web" middleware group. Make something great!
+|
 */
 
-//Route::get('/print-name/{name?}', [TestController::class, 'printName'])->name('print-my-name-route');
-
-// Routes groups
-//Route::controller(TestController::class)->prefix('test')->name('test.')
-//    ->middleware('check_zeina')->group(function () {
-//        Route::get('/zeina', 'zeina');
-//        Route::get('/print-name/{name?}', 'printName')->name('print-my-name-route');
-//});
-
-//Route::get('/hello', [UserController::class, 'index']);
-
-//Route::resource('posts', PostController::class);
-
-// Middleware
-//Route::get('/hello', SingleController::class)
-//    ->middleware('check_zeina');
-
-//Route::get('/hello', SingleController::class);
-
-// Theme Routes
-Route::controller(ThemeController::class)->name('theme.')->group(function () {
-    Route::get('/about', 'about')->name('about');
-    Route::get('/services', 'services')->name('services');
-    Route::get('/contact', 'contact')->name('contact');
-    Route::post('/contact/store', 'store')->name('contact.store');
-    Route::get('/displayContacts', 'display')->name('display');
+Route::get('/', function () {
+    return view('welcome');
 });
+
+Route::get('/dashboard', function () {
+    return view('dashboard');
+})->middleware(['auth', 'verified'])->name('dashboard');
+
+Route::middleware('auth')->group(function () {
+    Route::get('/profile', [ProfileController::class, 'edit'])->name('profile.edit');
+    Route::patch('/profile', [ProfileController::class, 'update'])->name('profile.update');
+    Route::delete('/profile', [ProfileController::class, 'destroy'])->name('profile.destroy');
+});
+
+require __DIR__.'/auth.php';
